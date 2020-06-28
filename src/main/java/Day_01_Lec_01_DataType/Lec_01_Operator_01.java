8df43e80a12db2301c5a03757f513048b37e68da
easywayautomation <easywayautomation@gmail.com>
2020-06-28 13:08:43 -0400

    Day01

---
+++ src/main/java/Day_01_Lec_01_DataType/Lec_01_Operator_01.java
@@ -0,0 +1,28 @@
+package Day_01_Lec_01_DataType;
+
+public class Lec_01_Operator_01 
+{
+	static int x=4; 
+	static int y=5;
+													
+	public static void main(String[] args) {
+		
+		মেথড_১();
+		মেথড_২();
+		
+	}
+	
+	public static void মেথড_১() {
+		System.out.println("Value of "+x);
+	}
+	
+	public static void মেথড_২() {
+		System.out.println("Value of "+y);
+	}
+	
+	
+	
+	
+	
+
+}
